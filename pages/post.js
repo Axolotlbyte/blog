0843b39a65195ca6d1c2088a5de448001fb3a626
axolotlbyte <arjaankhan@gmail.com>
2022-03-30 19:09:34 +0530

    fixes

--- pages/post.js
+++ pages/post.js
@@ -14,7 +14,7 @@ const post = () => {
             </div>
             <p className="text-lg">
               Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry's standard dummy text
+              industry. Lorem Ipsum has been the industry&apos;s standard dummy text
               ever since the 1500s, when an unknown printer took a galley of
               type and scrambled it to make a type specimen book. It has
               survived not only five centuries, but also the leap into
@@ -26,10 +26,10 @@ const post = () => {
               a reader will be distracted by the readable content of a page when
               looking at its layout. The point of using Lorem Ipsum is that it
               has a more-or-less normal distribution of letters, as opposed to
-              using 'Content here, content here', making it look like readable
+              using &apos;Content here, content here&apos;, making it look like readable
               English. Many desktop publishing packages and web page editors now
               use Lorem Ipsum as their default model text, and a search for
-              'lorem ipsum' will uncover many web sites still in their infancy.
+              &apos;lorem ipsum&apos; will uncover many web sites still in their infancy.
               Various versions have evolved over the years, sometimes by
               accident, sometimes on purpose (injected humour and the like).
               Where does it come from? Contrary to popular belief, Lorem Ipsum
@@ -40,13 +40,18 @@ const post = () => {
               consectetur, from a Lorem Ipsum passage, and going through the
               cites of the word in classical literature, discovered the
               undoubtable source. Lorem Ipsum comes from sections 1.10.32 and
-              1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good
+              1.10.33 of &quot;de Finibus Bonorum et Malorum&quot; (The Extremes of Good
               and Evil) by Cicero, written in 45 BC. This book is a treatise on
               the theory of ethics, very popular during the Renaissance. The
-              first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes
+              first line of Lorem Ipsum, &quot;Lorem ipsum dolor sit amet..&quot;, comes
               from a line in section 1.10.32.
             </p>
           </div>
+        </div>
+      </div>
+      <div id="comments" className="w-11/12 pt-4 mx-auto h-auto">
+        <div className="h-12 w-12 rounded-xl ring-2 ring-gray-300">
+
         </div>
       </div>
       <style jsx>{`
